a1d5d3cb2bd98b10a3a732abcb94643dd283aa28
Divyarajalakshmi Natarajan <divyarajalakshmin@gmail.com>
2016-04-27 14:23:49 -0700

    FINAL WORKING COPY.
    Get the businesses in San Jose, CA that are still “in business”. Results should be sorted by review counts in descending order. Return for the top 7 businesses, the business id, name, review count, and average rating of the business.

--- query 1.sql
+++ query 1.sql
@@ -1,3 +1,3 @@
-select * from (select business_id from business
-where city='San Jose' and state='CA' and status='T') B , (select count(Review_id) as numberOfReviews,Business_id from Reviews group by Business_id order by numberOfReviews desc)R 
+select B.Business_Id,B.Business_Name,R.REVIEW_COUNT,R.AVERAGE_RATING FROM (select business_id,BUSINESS_NAME from business
+where city='San Jose' and state='CA' and status='T') B , (select count(Review_id) as REVIEW_COUNT,Avg(RATING) AS AVERAGE_RATING, Business_id from Reviews group by Business_id order by REVIEW_COUNT desc)R 
 where B.Business_id = R.Business_id and rownum <=7;
